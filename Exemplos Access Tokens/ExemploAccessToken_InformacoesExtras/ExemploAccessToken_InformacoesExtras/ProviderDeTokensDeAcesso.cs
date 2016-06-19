da9f35876965c0fb9f7095cea82384d415db0107
Paulo Roberto Ramos de Andrade <paulo.roberto@randrade.net>
2016-06-19 12:02:17 -0300

    Adição de novo projeto
    Adicionando projeto de informações extras em access tokens

---
+++ Exemplos Access Tokens/ExemploAccessToken_InformacoesExtras/ExemploAccessToken_InformacoesExtras/ProviderDeTokensDeAcesso.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ExemploAccessToken_InformacoesExtras.Usuarios;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
+
+namespace ExemploAccessToken_InformacoesExtras
+{
+    public class ProviderDeTokensDeAcesso : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication
+            (OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials
+            (OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // encontrando o usuário
+            var usuario = BaseUsuarios
+                .Usuarios()
+                .FirstOrDefault(x => x.Nome == context.UserName 
+                                && x.Senha == context.Password);
+
+            // cancelando a emissão do token se o usuário não for encontrado
+            if (usuario == null)
+            {
+                context.SetError("invalid_grant", 
+                    "Usuário não encontrado um senha incorreta.");
+                return;
+            }
+
+            // emitindo o token com informacoes extras
+            // se o usuário existe
+            var identidadeUsuario = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            foreach (var funcao in usuario.Funcoes)
+                identidadeUsuario.AddClaim(new Claim(ClaimTypes.Role, funcao));
+            
+            context.Validated(identidadeUsuario);
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            // repassando propriedades do token, mas de forma aberta
+            foreach (var item in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(item.Key, item.Value);
+            }
+
+            // repassando claims que estão no token, mas de forma aberta
+            var claims = context.Identity.Claims
+                .GroupBy(x => x.Type)
+                .Select(y => new { Claim = y.Key, Values = y.Select(z => z.Value).ToArray() });
+
+            foreach (var claim in claims)
+            { 
+                context.AdditionalResponseParameters.Add(claim.Claim, JsonConvert.SerializeObject(claim.Values));
+            }            
+
+            // repassando informacões fixas
+            context.AdditionalResponseParameters.Add("info1", "valor");
+            context.AdditionalResponseParameters.Add("info2", 1);
+
+            return base.TokenEndpoint(context);
+        }
+
+    }
+}
---
+++ Exemplos Access Tokens/ExemploAccessToken_InformacoesExtras/ExemploAccessToken_InformacoesExtras/Usuarios/BaseUsuarios.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ExemploAccessToken_InformacoesExtras.Usuarios
+{
+    // simulacao de acesso a uma base de usuarios
+    public static class BaseUsuarios
+    {
+        public static IEnumerable<Usuario> Usuarios()
+        {
+            return new List<Usuario>
+            {
+                new Usuario { Nome = "Fulano", Senha = "1234",
+                    Funcoes = new string[] { Funcao.UsuarioNormal } },
+                new Usuario { Nome = "Beltrano", Senha = "5678",
+                    Funcoes = new string[] { Funcao.Gerente } },
+                new Usuario { Nome = "Sicrano", Senha = "0912",
+                    Funcoes = new string[] { Funcao.Administrador,
+                                                Funcao.Gerente } },
+            };
+        }
+    }
+}
